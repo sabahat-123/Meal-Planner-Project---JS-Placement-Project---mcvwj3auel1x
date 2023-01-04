0b77360251e33bd335e2f70fc23e284e960bb8e0
sabahat-123 <sabahat.parveen@imtspl.net>
2023-01-04 18:05:20 +0530

    update all

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ if(close){
         nav.classList.remove('active');
     })
 }
+// create variable  
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
 const age = document.getElementById("age");
@@ -28,28 +29,8 @@ function generateMeal(event){
     const url =`https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day`;
 
     fetch(url).then((response)=>response.json()).then((data)=>{
-    let bmrMale = 66.47 + (13.75 * weight.value) + (5.003 * height.value) - (6.755 * age.value);
-    let bmrFemale = 655.1 + (9.563 * weight.value) + (1.850 * height.value) - (4.676 * age.value);
-    if (gender.value === "Male" && activity.value === "light") {
-        var calories = bmrMale * 1.375;
-    }
-    else if (gender.value === "Male" && activity.value === "moderate") {
-        var calories = bmrMale * 1.55;
-    }
-    else if (gender.value === "Male" && activity.value === "active") {
-        var calories = bmrMale * 1.725;
-    }
-    else if (gender.value === "Female" && activity.value === "light") {
-        var calories = bmrFemale * 1.375;
-    }
-    else if (gender.value === "Female" && activity.value === "moderate") {
-        var calories = bmrFemale * 1.55;
-    }
-    else if (gender.value === "Female" && activity.value === "active") {
-        var calories = bmrFemale * 1.725;
-    }
         console.log(data);
-    
+        console.log("hiii");
         var mealName = "";
         for(let i=0; i<3; i++){
             if(i==0){
@@ -59,7 +40,7 @@ function generateMeal(event){
             }else{
                 mealName = "Dinner";
             }
-        const newDiv = document.createElement('div');
+        const newSec = document.createElement('section');
         const htmlStr = `<div class="card" style="width: 18rem; margin: 40px;">
         <img class="card-img-top" src="./image/img10.${data.meals[i].imageType}" alt="Card image cap">
         <div class="card-body">
@@ -69,9 +50,9 @@ function generateMeal(event){
             <a href="#" class="btn btn-primary" style="background-color: #f0754f; border: none;">Get Recipes</a>
         </div>
         </div>`;
-        const newCart = document.getElementById('cart');
-        newDiv.innerHTML = htmlStr;
-        newCart.appendChild(newDiv);
+        const newCard = document.getElementById('meals__container');
+        newSec.innerHTML = htmlStr;
+        newCard.appendChild(newSec);
         }
        
     }).catch((error)=>{
