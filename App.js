9e36cd2c0255c9b3c70f05d075092331b99a9edc
sabahat-123 <sabahat.parveen@imtspl.net>
2023-01-05 17:24:37 +0530

    update html

--- App.js
+++ App.js
@@ -18,19 +18,61 @@ const weight = document.getElementById("weight");
 const age = document.getElementById("age");
 const gender = document.getElementById("gender");
 const activity = document.getElementById("activity");
-// const generateMealBtn = document.getElementById("btn");
-// const box_1 = document.getElementsByClassName("box1");
+const form = document.getElementById("form");
+const sub_btn = document.getElementById("submit-btn");
+const newCard = document.getElementById('meals__container');
+const recipe_container = document.getElementById("recipie__container");
+const breakFast_img ="https://cdn6.projectmealplan.com/wp-content/uploads/2018/11/bacon-egg-breakfast-sandwiches-stacked-edit-257x257.jpg";
+const lunch_img ="https://cdn5.projectmealplan.com/wp-content/uploads/2021/09/cashew-chicken-stir-fry-meal-prep-2021-containers-top-sq-217x217.jpg";
+const dinner_img = "https://cdn4.projectmealplan.com/wp-content/uploads/2021/12/air-fryer-boneless-bbq-chicken-thighs-hero-top-217x217.jpg";
+const link_img = [breakFast_img,lunch_img,dinner_img];
 
-// api key
+//api key
 const apiKey ="2cd2da308f344968b58bb7de9aaa706b";
-//https://api.spoonacular.com/mealplanner/generate?apiKey=2cd2da308f344968b58bb7de9aaa706b&timeFrame=day
+// https://api.spoonacular.com/mealplanner/generate?apiKey=2cd2da308f344968b58bb7de9aaa706b&timeFrame=day
+//form validation
+form.addEventListener("submit" ,generateMeal)
+// calcutating BMR
+async function calBMR(weight,height,age){
+    let bmrMale = 66.47 + (13.75 * weight) + (5.003 *height) - (6.755 * age);
+    let bmrFemale =  655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
+    if(gender.value ==="male" && activity.value==="Light"){
+        let calories = bmrMale*1.375;
+        console.log(calories);
+    }
+    else if(gender.value ==="male" && activity.value==="Moderate"){
+        let calories = bmrMale*1.55;
+        console.log(calories);
+    }
+    else if(gender.value ==="male" && activity.value==="Active"){
+        let calories = bmrMale*1.725;
+        console.log(calories);
+    }
+    else if(gender.value ==="Female" && activity.value==="Light"){
+        let calories = bmrFemale*1.375;
+        console.log(calories);
+        
+    }
+    else if(gender.value ==="Female" && activity.value==="Moderate"){
+        let calories = bmrFemale*1.55;
+        console.log(calories);
+    }
+    else{
+        let calories = bmrFemale*1.725;
+        console.log(calories); 
+    }
+   
+}
+//generate the meals
 function generateMeal(event){
-
+    event.preventDefault();
     const url =`https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day`;
+
+    newCard.innerHTML="";
 
     fetch(url).then((response)=>response.json()).then((data)=>{
         console.log(data);
-        console.log("hiii");
+        console.log(link_img);
         var mealName = "";
         for(let i=0; i<3; i++){
             if(i==0){
@@ -40,17 +82,17 @@ function generateMeal(event){
             }else{
                 mealName = "Dinner";
             }
-        const newSec = document.createElement('section');
+        let newSec = document.createElement('section');
         const htmlStr = `<div class="card" style="width: 18rem; margin: 40px;">
-        <img class="card-img-top" src="./image/img10.${data.meals[i].imageType}" alt="Card image cap">
+        <img class="card-img-top" src="${link_img[i]}" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">${mealName}</h5>
-            <p class="card-text"><span style="font-weight: 600;">${data.meals[i].title}</span><br>calories : ${data.nutrients.calories}<br>
+            <p class="card-text"><span style="font-weight: 600;">${data.meals[i].title}</span><br>calories :<br>
             </p>
-            <a href="#" class="btn btn-primary" style="background-color: #f0754f; border: none;">Get Recipes</a>
+            <a href="#" class="btn btn-primary" style="background-color: #f0754f; border: none;" onclick="getRecipe(${data.meals[i].id})">Get Recipes</a>
         </div>
         </div>`;
-        const newCard = document.getElementById('meals__container');
+     
         newSec.innerHTML = htmlStr;
         newCard.appendChild(newSec);
         }
@@ -59,4 +101,28 @@ function generateMeal(event){
         console.log(error);
     })
 
+}
+
+
+// function for fetch recipie
+async function getRecipe(id){
+    console.log(id);
+    const recipie =`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}&includeNutrition=false`;
+    fetch(recipie).then((response)=>response.json()).then((getMeal)=>{
+        let newSec = document.createElement('section');
+         const table_str =` <table class="table table-striped">
+         <thead>
+           <tr>
+             <th scope="col"> Ingredients</th>
+             <th scope="col">steps</th>
+             <th scope="col">Equipment</th>
+           </tr>
+         </thead>
+       </table>`
+       newSec.innerHTML = table_str;
+       recipe_container.appendChild(newSec);
+        console.log(getMeal);
+    }).catch((error)=>{
+        console.log(error);
+    })
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# html-css-js-project-boilerplate
+<h1 id="heading" align="center">Meal Planner</h1>
+<div id="header" align="center">
+  <img src="https://www.smallfootprintfamily.com/wp-content/uploads/meal-planning-and-the-environment.jpg" width="50%
+  "/>
+</div>
